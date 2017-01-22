07d8fd8066fc5d37a83363bbf8ea30576adf781b
Rinat Yuldashbaev <rinatoptimus@gmail.com>
2017-01-22 17:39:15 +0300

    1st commit.

---
+++ basic/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    // 'class' => 'yii\db\Connection',
+
+    // test begin
+    'class' => 'yii\db\Connection',
+    // test end
+
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
